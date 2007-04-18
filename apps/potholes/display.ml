c450822d199c895d88cd2e0679a77ebaedc43523
Ryan Newton <rrnewton@gmail.com>
2007-04-18 21:55:29 +0000

    intermediate checkin (brokenrev)

--- apps/potholes/display.ml
+++ apps/potholes/display.ml
@@ -1,53 +1,55 @@
-
+open Utils;;
 open Graphics;;
 open Printf;;
 open Bigarray;;
 
+
+(* 
+  HACK HACK HACK.. DUPLICATED CODE AND PARAMETER VALUES:
+   MANUALLY MUST SET THE RIGHT PARAMETERS:
+  *)
+
+(*
+let (gridsize, shiftx, shifty, maxx, maxy, kind) = 0.001, 2000, 2500, 4096, 4096, int16_unsigned;;    (* APPROX 316 feet *)
+let (gridsize, shiftx, shifty, maxx, maxy, kind) = 0.0007, 1900, 2100, 4096, 4096, int16_unsigned;;
+let (gridsize, shiftx, shifty, maxx, maxy, kind) = 0.0001, 900, 3700, 8192, 8192, int16_unsigned;;    (* smallgrid *)
+*)
+
+let (gridsize, shiftx, shifty, maxx, maxy, kind) = 0.0001, 900, 3700, 8192, 8192, int16_unsigned;;    (* smallgrid *)
+
+
+
 if Array.length Sys.argv == 1
 then (raise (Failure "must pass data filename as first command line argument"));;
 
+
+(*
+let maxx = 
+  if Array.length Sys.argv > 2
+  then int_of_string Sys.argv.(2)
+  else 4096
+let maxy = 
+  if Array.length Sys.argv > 3
+  then int_of_string Sys.argv.(3)
+  else 4096
+    *)
       
 open_graph "";;
 auto_synchronize false;;
 resize_window 800 800;;
 set_window_title (Sys.argv.(1));;
 
-(* 
-  HACK HACK HACK.. DUPLICATED CODE AND PARAMETER VALUES:
-  *)
+printf "Running script to generated CDF and display stored bitmap.\n";;
+flush stdout;;
 
-let shiftx = 2000
-let shifty = 2500
-let maxx = 4096 
-let maxy = 4096
+scrollx := (maxx / 2);;
+scrolly := (maxy / 2 - 300);;
+the_maxx := maxx;;
+the_maxy := maxy;;
+
 (* Allow scrolling around with mouse. *)
 let scrollx = ref 0
 let scrolly = ref 0
-
-
-let bitarray n m = 
-  (*Array2.create int8_unsigned c_layout  n m;;*)
-  let p = Unix.openfile (Sys.argv.(1)) [Unix.O_RDWR; Unix.O_CREAT; Unix.O_TRUNC] 0o644  in
-    Array2.map_file p int8_unsigned c_layout true n m
-
-let get ba i j   = Array2.get ba i j
-let set ba i j x = Array2.set ba i j x
-let size_x ba    = Array2.dim1 ba 
-let size_y ba    = Array2.dim2 ba   
-
-
-
-let draw_screen arr = 
-  for i = 0 to size_x arr - 1 do
-    for j = 0 to size_y arr - 1 do
-      if 1 == get arr i j 
-      then plot (i - shiftx ) (j - shifty + 300)
-    done 
-  done;
-  printf "drawn!\n";
-  flush stdout;
-  synchronize ()
-;;
 
 
 let tofile fn ba = 
@@ -60,14 +62,71 @@ let tofile fn ba =
 
 let fromfile fn = 
   let p = Unix.openfile fn [Unix.O_RDONLY] 0o644  in
-  let f = Array2.map_file p int8_unsigned c_layout false maxx maxy in
-    draw_screen f
-    
+  let f = Array2.map_file p kind c_layout false maxx maxy in
+    f
 ;;
 
-(fromfile (Sys.argv.(1))) ;;
+let counthits arr = 
+  let sum = ref 0 in
+  for i = 0 to maxx-1 do
+    for j = 0 to maxy-1 do
+      if get arr i j != 0 
+      then incr sum
+    done 
+  done;
+  !sum
+
+let sumhits arr = 
+  let sum = ref 0 in
+  for i = 0 to maxx-1 do
+    for j = 0 to maxy-1 do
+      sum := !sum + get arr i j
+    done 
+  done;
+  !sum
+;;
+
+
+let buildhisto arr = 
+  let hist = Array.make 10000 0 in
+  for i = 0 to maxx-1 do
+    for j = 0 to maxy-1 do
+      let cnt = get arr i j in
+	hist.(cnt) <- 1 + hist.(cnt);
+    done 
+  done;
+    hist 
+;;
+
+
+
 (printf "YAY\n");;
+flush stdout;;
 
-input_line stdin;
+let arr = (fromfile (Sys.argv.(1))) ;;
 
+(printf "GOT ARR\n");; flush stdout;;
+printf "COUNT: %d\n" (counthits arr);; flush stdout;;
+printf "SUMHITS: %d\n" (sumhits arr);; flush stdout;;
 
+let hist = buildhisto arr;;
+
+printf "SUMHITS: %d\n" (sumhits arr);; flush stdout;;
+let histfile = open_out "./histogram.dat";;
+
+for i = 0 to Array.length hist - 1 do
+  if hist.(i) > 0
+  then begin 
+    fprintf histfile "%d %d\n" i hist.(i);
+    printf "%d:%d " i hist.(i)
+  end 
+done;;
+close_out histfile;;
+
+printf "\n\nWrote histogram to file histogram.dat\n";;
+
+(* Then just leave the picture up till the user kills us. *)
+while true do
+  update_scroll();
+  draw_screen arr;
+done
--- apps/potholes/tally_mileage_ALLCARS.ml
+++ apps/potholes/tally_mileage_ALLCARS.ml
@@ -1,18 +1,21 @@
 
 
 
-
 (*
-let (gridsize, shiftx, shifty) = 0.001, 2000, 2500;;    (* APPROX 316 feet *)
-let (gridsize, shiftx, shifty) = 0.00099, 2000, 2500;;
-let (gridsize, shiftx, shifty) = 0.0005, 2000, 2500;;   (* APPROX 158 feet *)
+let (gridsize, shiftx, shifty, maxx, maxy) = 0.001, 2000, 2500, 4096, 4096;;    (* APPROX 316 feet *)
+let (gridsize, shiftx, shifty, maxx, maxy) = 0.0007, 1900, 2100, 4096, 4096;;
+
 *)
 
-let (gridsize, shiftx, shifty) = 0.0007, 1900, 2100;;
+let (gridsize, shiftx, shifty, maxx, maxy) = 0.0001, 900, 3700, 8192, 8192;;   
+
+(* let smallgrid = "" *)
+let smallgrid = "_SMALLGRID"
 
 
 (* Lewis uses 0.0001 *) 
 
+open Utils;;
 open Graphics;;
 open Printf;;
 open Bigarray;;
@@ -22,114 +25,15 @@ resize_window 800 800;;
 
 printf "Running script to analyze all car traces.\n";;
 flush stdout;;
-    
-(********************************************************************************)
 
-(* simple bit arrays *)
+scrollx := (maxx / 2);;
+scrolly := (maxy / 2 - 300);;
 
+the_maxx := maxx;;
+the_maxy := maxy;;
 
-(*
-let bitarray n m = Array.make_matrix n m 0
-let get ba i j = ba.(i).(j)
-let set ba i j x = ba.(i).(j) <- x
-let xdim ba = Array.length ba
-let ydim ba = Array.length ba.(0)
-*)
-
-
-
-
-
-(* Let's try just using a mapped file from the start: *)
-let bitarray fn n m = 
-  (*Array2.create int8_unsigned c_layout  n m;;*)
-  let p = Unix.openfile fn [Unix.O_RDWR; Unix.O_CREAT; Unix.O_TRUNC] 0o644  in
-    Array2.map_file p int8_unsigned c_layout true n m
-
-let get ba i j   = Array2.get ba i j
-let set ba i j x = Array2.set ba i j x
-let xdim ba    = Array2.dim1 ba 
-let ydim ba    = Array2.dim2 ba   
-
-
-(*
-let bitarray n m = 
-  assert (n mod 16 == 0);
-  Array2.create int16_unsigned c_layout 
-    (int_of_float (ceil ((float_of_int n) /. 16.))) m;;
-let get ba i j = 
-  let word = Array2.get ba (i asr 4) j in
-    printf "setting i %d i_ind %d word %d\n" i (i asr 4) word;
-    (* annoying way to test a bit *)
-    1 land (word asr (i land 15))
-let set ba i j x = 
-  let word = Array2.get ba (i asr 4) j in
-    (* annoying way to test a bit *)
-    Array2.set ba (i asr 4) j 
-     (let bit = 1 asr (i land 15) in
-       (if x == 0
-	then word land (lnot bit)
-	else word lor bit))
-let xdim ba = Array2.dim1 ba * 16
-let ydim ba = Array2.dim2 ba   
-*)
-
-(********************************************************************************)
-
-
-let maxx = 4096 
-let maxy = 4096
-
-
-(* Allow scrolling around with mouse. *)
-let scrollx = ref (maxx / 2)
-let scrolly = ref (maxy / 2 - 300)
-
-let theworld = bitarray ("/home/newton/data/ALLCARS.bitmap") maxx maxy
-let distlog = open_out ("/home/newton/data/ALLCARS.dstlog")
-
-let draw_screen arr = 
-  clear_graph();
-(*   for i = 0 to xdim arr - 1 do *)
-(*     for j = 0 to ydim arr - 1 do *)
-(*       if 1 == get arr i j  *)
-(*       then plot (i - shiftx + !scrollx) (j - shifty + 300 + !scrolly) *)
-(*     done  *)
-(*   done; *)
-  for x = 0 to size_x()-1 do
-    for y = 0 to size_y()-1 do
-      let i = x + !scrollx in
-      let j = y + !scrolly in 
-      if 1 == get arr i j 
-      then plot x y
-    done 
-  done;  
-  printf "drawn! screen location: %d %d\n" (!scrollx) (!scrolly);
-  flush stdout;
-  synchronize ()
-;;
-
-
-let update_scroll = 
-  let lastmousepos = ref None in
-  fun () -> 
-    if button_down()
-    then 
-      let (oldx,oldy) = match !lastmousepos with
-          None ->
-	    let p = mouse_pos() in 
-	      lastmousepos := Some p; 
-	      p
-	|	Some p -> p 
-      in	
-      let (x,y) = mouse_pos() in 
-      let (dx,dy) = (x-oldx, y-oldy) in
-      printf "  SCROLLING: %d %d\n" dx dy;
-      scrollx := min (maxx - size_x()) (max 0 (!scrollx - dx));
-      scrolly := min (maxy - size_y()) (max 0 (!scrolly - dy));
-      lastmousepos := Some (x,y)
-    else lastmousepos := None
-
+let theworld = bitarray ("/home/newton/data/ALLCARS"^smallgrid^".bitmap") maxx maxy
+let distlog = open_out ("/home/newton/data/ALLCARS"^smallgrid^".dstlog")
 
 (* The number of steps forward spent moving.  In 400ths of a second.*)
 let stepsforward = ref Int64.zero
@@ -182,8 +86,9 @@ lon = 68.9 miles
 	 if yp<0 then (printf "y neg %d\n" xp; assert false);*)
 
 	 (* HACKISH: Here we just throw out the data *)
-	 if (*time < 4900000. (*|| xp < 0 || yp < 0 *) *)
-	    false 
+	 if time < 4900000. 
+	    || xp < 0 || yp < 0 
+            || xp >= maxx || yp >= maxy
 	 then loop lastx lasty 
 	 else 
 	   begin
@@ -196,8 +101,8 @@ lon = 68.9 miles
 	     );
 	     if !count mod 10000 == 0 
 	     then (
-	     	     update_scroll();
-		     (*printf "line read lat %f  lon %f   x %d y %d  xp %d  yp %d\n" lat lon x y xp yp; flush stdout;*)
+	     	 update_scroll();
+(*		 printf "line read lat %f  lon %f   x %d y %d  xp %d  yp %d\n" lat lon x y xp yp; flush stdout;*)
 		  );
 	     if !count mod 100000 == 0
 	     then (
@@ -232,7 +137,9 @@ lon = 68.9 miles
 			 thistime
 			 (*(!timemoving -. origin)*)
 			 !stepsforward !cellcount (*!sincelaststep*);
-		     );
+		     )
+		     else 
+		      set theworld xp yp (old + 1);
 		     (*sincelaststep := 0.0;*)
 	       end;
 	     incr count;
@@ -245,11 +152,7 @@ lon = 68.9 miles
 
 (*process_file Sys.argv.(1);;*)
 
-try process_file 0.0           "/home/newton/data/slave29.txt" with End_of_file -> 
-try process_file !lastfiletime "/home/newton/data/slave14.txt" with End_of_file -> 
-try process_file !lastfiletime "/home/newton/data/slave18.txt" with End_of_file -> 
-
-(*
+try process_file !lastfiletime "/home/newton/data/slave6.txt" with End_of_file -> 
 try process_file !lastfiletime "/home/newton/data/slave10.txt" with End_of_file -> 
 try process_file !lastfiletime "/home/newton/data/slave11.txt" with End_of_file -> 
 try process_file !lastfiletime "/home/newton/data/slave14.txt" with End_of_file -> 
@@ -259,7 +162,7 @@ try process_file !lastfiletime "/home/newton/data/slave19.txt" with End_of_file 
 try process_file !lastfiletime "/home/newton/data/slave22.txt" with End_of_file -> 
 try process_file !lastfiletime "/home/newton/data/slave24.txt" with End_of_file -> 
 try process_file !lastfiletime "/home/newton/data/slave29.txt" with End_of_file -> 
-*)
+
    printf "Finished.  Reached end of all files."
 ;;
 
