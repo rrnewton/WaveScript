e6c5c3380fc9cb64c23c31a3b96fea025595a347
Ryan Newton <rrnewton@gmail.com>
2007-11-12 16:42:19 +0000

    minor fix to wsc script

--- apps/marmot/refactored/runall.sh
+++ apps/marmot/refactored/runall.sh
@@ -5,10 +5,13 @@ export HANDOPT_BUILDSPLIT=true
 rm -rf ./temp
 mkdir temp
 
-for threads in `seq 1 16`; do
+for threads in `seq 2 16`; do
   export NUMTHREADS=$threads
-  wsc.new test_marmot2.ws -O3
-#  ramp_up_rate.sh temp/log 5 10 400 ./query.exe -j $threads -n 30 
+#  export NUMTHREADS=16
+#  wsc.new test_marmot2.ws -O3
+
+#  ramp_up_rate.sh temp/log 5 10 500 ./query.exe -j $threads -n 200 
+
   ramp_up_rate.sh temp/log 10 20 200 ./query.exe -j $threads -n 30 
   echo "# The above was for $threads threads." >> RESULTS.txt
   mv RESULTS.txt "./temp/RESULTS_"$threads".txt"
