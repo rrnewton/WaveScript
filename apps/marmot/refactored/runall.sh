1cf82669b1646247409daa81e141fdbdf7344b64
Lewis Girod <girod@nms.csail.mit.edu>
2007-11-15 11:49:31 +0000

    checking in lews changes to run script

--- apps/marmot/refactored/runall.sh
+++ apps/marmot/refactored/runall.sh
@@ -2,16 +2,26 @@
 
 unset HANDOPT_BUILDSPLIT
 unset HANDOPT_MAPSPLIT
+unset HANDOPT_FULLPAR
+unset DEEPCOPY
+unset DODUMMYAML
 
-#export HANDOPT_BUILDSPLIT=true
-export HANDOPT_MAPSPLIT=true
+# use hoard
+export LD_PRELOAD="/home/stoledo/libhoard.so:/lib64/libdl-2.6.so"
+
+export HANDOPT_BUILDSPLIT=true
+#export HANDOPT_MAPSPLIT=true
+#export HANDOPT_FULLPAR=truee
+xport DEEPCOPY=true
 
 rm -rf ./temp
 mkdir temp
 
 echo "#h numthreads realtime cputime" > RESULTS.txt 
 
-for threads in `seq 1 16`; do
+export GRIDSIZE=3600
+
+for threads in `seq 0 15`; do
   export NUMTHREADS=$threads
 #  export NUMTHREADS=16
 
@@ -25,10 +35,14 @@ if [ ! true ]; then
 
 else
   wsc.new test_marmot2.ws -O3 -t -dot
-  ./query.exe -j $threads -n 30 > temp/LOG_$threads
-  export TMP=`$REGIMENTD/bin/extract_startend_real.sh temp/LOG_$threads`
-  export TMP2=`$REGIMENTD/bin/extract_startend_times.sh temp/LOG_$threads`
-  echo "$threads  $TMP  $TMP2" >> RESULTS.txt
+  TPLUSONE=`expr $threads + 1`
+#  for index in `seq 1 10` ; do 
+    echo running with -j $TPLUSONE
+    ./query.exe -j $TPLUSONE -n 30 > temp/LOG_$threads
+    export TMP=`$REGIMENTD/bin/extract_startend_real.sh temp/LOG_$threads`
+    export TMP2=`$REGIMENTD/bin/extract_startend_times.sh temp/LOG_$threads`
+    echo "$threads  $TMP  $TMP2  $index" >> RESULTS.txt
+#  done
 #  mv RESULTS.txt "./temp/RESULTS_"$threads".txt"
 fi
 
