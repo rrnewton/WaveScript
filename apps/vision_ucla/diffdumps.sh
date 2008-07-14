a469a50e8829d23fcc17200b1b2183e8a637f263
no author <noone@nowhere>
2008-07-14 18:05:45 +0000

    Wow, this is very disturbing.
    Currently, switching between flat vs. nested 3d matrix representations changes the output 
    slightly!  I see a small amount of one bit differences in the output (just as our 
    original difference from Teresa's int code).

--- apps/vision_ucla/diffdumps.sh
+++ apps/vision_ucla/diffdumps.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-PREFIX=bhatta
+#PREFIX=bhatta
+#preindex=000
+
+prefix=my_int
+preindex=_
 
 hexdump processed/Diff_0.bmp > processed/dumpws.txt
-hexdump processed/FeederStation_2007-06-26_14-00-03.000/"$PREFIX"3_20_1_0_128_16_2_2_30_0_bl/Diff_FeederStation_2007-06-26_14-00-03.000_00000000.jpg.bmp > processed/dumpcpp.txt
+#hexdump processed/Feeder*/"$PREFIX"*/Diff_FeederStation_2007-06-26_14-00-03.000_00000000.jpg.bmp > processed/dumpcpp.txt
+hexdump processed/Feeder*/"$PREFIX"*/Diff*"$preindex"0.*bmp > processed/dumpcpp.txt
 diff processed/dumpws.txt processed/dumpcpp.txt
