902c217a5914205b5a41ed36381c6a29ad3e084f
no author <noone@nowhere>
2008-07-25 18:59:57 +0000

    Oops, my favorite error.  I forgot to checkin atomic_incr.

---
+++ src/linked_lib/atomic_incr_intel.h
@@ -0,0 +1,36 @@
+
+
+
+inline int atomic_exchange_and_add( int * pw, int dv )
+{
+    // int r = *pw;
+    // *pw += dv;
+    // return r;
+
+    int r;
+
+    __asm__ __volatile__
+    (
+        "lock\n\t"
+        "xadd %1, %0":
+        "=m"( *pw ), "=r"( r ): // outputs (%0, %1)
+        "m"( *pw ), "1"( dv ): // inputs (%2, %3 == %1)
+        "memory", "cc" // clobbers
+    );
+
+    return r;
+}
+
+inline void atomic_increment( int * pw )
+{
+    //printf("Atomic Incr %p\n", pw);
+    //atomic_exchange_and_add( pw, 1 );
+    __asm__
+    (
+        "lock\n\t"
+        "incl %0":
+        "=m"( *pw ): // output (%0)
+        "m"( *pw ): // input (%1)
+        "cc" // clobbers
+    );
+}
