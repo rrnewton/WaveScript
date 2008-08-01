e65b2d057a6bb86edbfebbda2e26128994037e62
no author <noone@nowhere>
2008-08-01 17:53:55 +0000

    Here I'm being ambitious.  The last rev hasn't passed yet.  But I believe I've got datareps working, so I added that back in.
    That's everything for the perfreport.  Now for some new stuff (parallel benchmarks).

--- benchmarks/datareps/plot.gp
+++
@@ -1,7 +0,0 @@
-
-set title "Data Representation: Allocating Arrays of Arrays" 
-#set yrange [ 0.00000 : 46000. ] noreverse nowriteback
-load "../shared.gp"
-#set style histogram clustered gap 1 title offset character 0, 0, 0
-#plot 'RESULTS.txt' using 2:xtic(1) title col, '' using 3 title col, '' using 4 title col
-plot 'alloc_arrarr.result' using 2:xtic(1) title col, '' using 3 title col, '' using 4 title col
--- benchmarks/datareps/plotall.sh
+++ benchmarks/datareps/plotall.sh
@@ -3,45 +3,28 @@
 
 source ../shared.sh
 
-function go3() {
+function go() {
   REP=$1
   RANGE=$2
   PLOT=plot_"$REP".gp
+  #FIELDS=`cat $REP.result | wc -l`
+  FIELDS=`tail -n 1 $REP.result | wc -w`
+  export BACKENDS="`seq 1 $((FIELDS - 1))`"
+  echo BACKENDS : $BACKENDS
   echo set title \"Data Representation: $1\"           > $PLOT
-  set "yrange [ 0.00000 : $2 ] noreverse nowriteback" >> $PLOT
+  echo "set yrange [ 0.00000 : $2 ] noreverse nowriteback" >> $PLOT
   dump_plot_script $PLOT $REP.result
   gnuplot $PLOT > $1.eps
 }
 
-# function go3() {
-#  echo "Plotting..."
-#   echo 'set title "Data Representation: '$1'"
-#   set yrange [ 0.00000 : '$2' ] noreverse nowriteback 
-#   load "../shared.gp"
-#   plot "'$1'.result" using 2:xtic(1) title col, "" using 3 title col, "" using 4 title col; ' \
-#  | gnuplot > $1.eps
-# }
+go alloc_arrarr 46000.
+go  fold_arrarr 46000.
 
-# function go2() {
-#  echo "Plotting..."
-# echo 'set title "Data Representation: '$1'";
-#       set yrange [ 0.00000 : '$2' ] noreverse nowriteback;
-#       load "../shared.gp";
-#       plot "'$1'.result" using 2:xtic(1) title col, "" using 3 title col; ' \
-#     | gnuplot > $1.eps
-# }
-
-go3 alloc_arrarr 46000.
-
-exit 
-
-go3  fold_arrarr 46000.
-
-go2 alloc_arraytuple 22000.
-go2  fold_arraytuple 22000.
-go2 alloc_tuplearray 22000.
-go2  fold_tuplearray 22000.
-
+go alloc_arraytuple 22000.
+go  fold_arraytuple 22000.
+go alloc_tuplearray 22000.
+go  fold_tuplearray 22000.
+ 
 echo "Convert all to pdf."
 ls *.eps | xargs -n1 ps2pdf
 
