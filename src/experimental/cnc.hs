da93ce4e4acc42e925ffe0b4d578f8474f3d6b84
no author <noone@nowhere>
2009-07-27 15:23:20 +0000

    This simple serial version works.

--- src/experimental/cnc.hs
+++ src/experimental/cnc.hs
@@ -5,12 +5,9 @@
 
 import Control.Monad.ST
 import Data.STRef
-import  Data.Set as Set
-import  Data.Map as Map
-
+import Data.Set as Set
+import Data.Map as Map
 import System.IO.Unsafe
-
-type Dummy = ()
 
 -- A tag collection has a buffer of newly written tags.
 -- It also has a unique identifier.
@@ -26,10 +23,6 @@ type Step s a = a -> CncCode s ()
 -- I think that means I can use something degenerate for the
 -- state-thread-distinguishing type arguument.
 type CncCode s a = ST s a
-
---instance Monad CncCode where
---    Hmm [a] >>= f  =  f a
---    return x = Hmm [x]
 
 counter = newSTRef 0
 
@@ -54,10 +47,11 @@ put col tag item =
     do map <- readSTRef col
        writeSTRef col (Map.insert tag item map)
 
-get :: Ord a => ItemCol s a b -> a      -> CncCode s b
+get :: (Ord a, Show a) => ItemCol s a b -> a      -> CncCode s b
 get col tag = 
     do map <- readSTRef col
-       return $ Map.findWithDefault undefined tag map
+       return $ Map.findWithDefault 
+		  (error ("get item: tag not found " ++ show tag)) tag map
 
 
 -- Tricky existential types:
@@ -85,51 +79,61 @@ prescribe (Graph graph) (MP (tagc,step)) =
        return ()
 
 runSimpleScheduler (Graph graph) = 
-    do 
-          stepmap <- readSTRef graph
-	  -- For each tag collection:
-	  Map.fold (\ ls acc -> 
+    do    stepmap <- readSTRef graph
+	  unsafeIOToST $ putStr ("Size of map " ++ show (Map.size stepmap) ++"\n")
+	  allempty <-
+	   Map.fold (\ ls acc ->   -- For each tag collection:
 		    case head ls of
 		     (MP (tags,_)) -> 
 		        do (id,frnt,bck) <- readSTRef tags
+		           bool <- acc
+		           let newbool = bool && Set.null frnt
 		           -- For each step prescribed by this tag collection:
 		           foldr (\ (MP (tags,step)) (acc) ->
 				  do (id,frnt,bck) <- readSTRef tags
-				     -- Invoke each step:
+				     -- Invoke the step:
 				     Set.fold (\ tag acc -> do acc; step tag)
 				              acc frnt)
-		                 acc ls
+		                 (return ()) ls
 		           -- After we're done consuming the fresh tags, clear them:
 		           writeSTRef tags (id, Set.empty, Set.union frnt bck)
-		           unsafeIOToST $ putStr "Hello\n"
-		           return undefined)
-		   (return ())
-         	   stepmap
-          return ()
---       return ()
-	  
+		           unsafeIOToST $ putStr ("Finished tag collection " ++ show id ++"\n")
+		           return newbool)
+		   (return True)
+         	   stepmap	  
+	  if allempty
+	   then unsafeIOToST $ putStr ("All tag collection inboxes empty.  Finished.\n")
+	   else runSimpleScheduler (Graph graph)
 
 
--- System.IO.Unsafe.unsafePerformIO
+--------------------------------------------------------------------------------
 
+incrStep d1 (t2,d2) tag = 
+ do val <- get d1 tag 
+    unsafeIOToST $ putStr ("  ("++ show tag ++") Incrementing " ++ show val ++"\n")
+    put d2 tag (val + 1)
+    call t2 tag
 
-test = 
+test = -- Allocate collections:
     do t1 <- newTagCol 0
        t2 <- newTagCol 1
+       t3 <- newTagCol 2
        d1 <- newItemCol
        d2 <- newItemCol
+       d3 <- newItemCol
        -- Initialize:
        put d1 'a' 33
+       put d1 'b' 100
        call t1 'a'
-       let step1 t = 
-	       do val <- get d1 t 
-		  put d2 t (val + 1)
-		  call t2 t
+       call t1 'b'
+       -- Build and execute the graph:
        graph <- newGraph
-       prescribe graph (MP (t1,step1))		 
+       prescribe graph (MP (t1, incrStep d1 (t2,d2)))		 
+       prescribe graph (MP (t2, incrStep d2 (t3,d3)))
        runSimpleScheduler graph
-       result <- get d2 'a'
-       return result 
+       result1 <- get d3 'a'
+       result2 <- get d3 'b'
+       return (result1, result2) 
 
 
 go = runST test
@@ -146,3 +150,7 @@ ghci -XRankNTypes cnc.hs
 ghci -fglasgow-exts cnc.hs
 
 -}
+
+--instance Monad CncCode where
+--    Hmm [a] >>= f  =  f a
+--    return x = Hmm [x]
