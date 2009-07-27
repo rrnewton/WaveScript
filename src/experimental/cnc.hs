b95f6c0096cc47069cacfcc5173813c28a03d0c9
no author <noone@nowhere>
2009-07-27 05:02:48 +0000

    Progress.

--- src/experimental/cnc.hs
+++ src/experimental/cnc.hs
@@ -60,12 +60,11 @@ get col tag =
        return $ Map.findWithDefault undefined tag map
 
 
-type MatchedPair s = (forall t. Ord t => (TagCol s t, Step s t))
---type Graph s = Ord TagColID => STRef s (forall t. Ord s => Map TagColID [Step s t])
---type Graph s = STRef s (forall t. Ord t => Map TagColID [Step s t])
---type Graph s = STRef s (Map TagColID (forall t. Ord t => [Step s t]))
+-- Tricky existential types:
+-- Here's a tag collection and associated step that "match" tag-types:
+data MatchedPair s = forall t. Ord t => MP (TagCol s t, Step s t)
+
 data Graph s = Graph (STRef s (Map TagColID [MatchedPair s]))
---type Graph s = forall t. Ord t => STRef s (Map TagColID ([Step s t]))
 
 -- Do we need a different monad for wiring together the graph??
 -- Not in this version...
@@ -76,45 +75,27 @@ newGraph :: CncCode s (Graph s)
 newGraph = do ref <- newSTRef (Map.empty :: Map TagColID [MatchedPair s])
 	      return (Graph ref)
 
---STRef s a -> t -> t1 -> ST s ()
--- HUH -- why Ord s and not ord t???
---prescribe :: Ord s => Graph s -> TagCol s t -> Step s t -> CncCode s ()
---prescribe :: Ord tg => Graph s -> TagCol s tg -> Step s tg -> CncCode s ()
-
---prescribe :: Graph s -> TagCol s tg -> Step s tg -> CncCode s ()
-
---forall s t1 a t2 t3 t.
---             (Ord t1) =>
---             STRef s (Map t1 [a]) -> STRef s (t1, t2, t3) -> t -> ST s ()
-
---prescribe :: forall s t a t1 t2.
---             (Ord t) =>
---             STRef s (Map t [a]) -> STRef s (t, t1, t2) -> a -> ST s ()
-
---             STRef s (Map t [a]) -> STRef s (t, t1, t2) -> a -> ST s ()
-
---prescribe :: 
---	     STRef s (Map t [a]) -> STRef s (t, t1, t2) -> a -> CncCode s ()
----	     Graph s             -> STRef s (t, t1, t2) -> a -> CncCode s ()
---	     STRef s (Map t [a]) -> TagCol s t1         -> a -> ST s ()
---	     STRef s (Map TagColID [a]) -> TagCol s t1  -> a -> ST s ()
---	     STRef s (Map TagColID [(forall t. Ord t => Step s t)]) -> TagCol s t1  -> a -> ST s ()
-
---prescribe :: Graph s -> TagCol s tg -> Step s tg -> CncCode s ()
 prescribe :: Graph s -> MatchedPair s -> CncCode s ()
---prescribe (Graph graph) tagc (step) = 
-prescribe (Graph graph) (tagc,step) = 
+prescribe (Graph graph) (MP (tagc,step)) = 
     do stepmap <- readSTRef graph
        (id,frnt,bck) <- readSTRef tagc
        let ls = Map.findWithDefault ([]::[MatchedPair s]) id stepmap 
---       writeSTRef graph stepmap
---       writeSTRef graph (Map.insert id ls stepmap)
---       writeSTRef graph (Map.insert id [step] stepmap)
---       let pr :: forall s. MatchedPair s = (tagc,step) 
-       let pr = (tagc,step) 
---       writeSTRef graph (Map.insert id (pr:ls) stepmap)
---       return stepmap
---       return pr
+       let pr = MP (tagc,step) 
+       writeSTRef graph (Map.insert id (pr:ls) stepmap)
+       return ()
+
+runSimpleScheduler (Graph graph) = 
+    do stepmap <- readSTRef graph
+       let doEach _ acc = acc
+       let doList (MP (tags,step)) acc = 
+	     do (id,frnt,bck) <- readSTRef tags 
+		val <- acc
+	        return (if Set.null frnt
+			then val 
+			else val)
+       let foo = Map.fold (\ ls acc -> foldr doList acc ls)
+		          (return 99)
+         		  stepmap
        return ()
 
 
@@ -133,12 +114,14 @@ test =
 	       do val <- get d1 t 
 		  put d2 t (val + 1)
 		  call t2 t
-       sched <- newGraph
---       prescribe sched t1 step1
-       return ()
+       graph <- newGraph
+       prescribe graph (MP (t1,step1))		 
+       runSimpleScheduler graph
+       result <- get d2 'a'
+       return result 
 
 
---go = runST test
+go = runST test
 
 
 --type T = Int
