d72f34487e3835c55ef4c557d51017449d7abd0f
no author <noone@nowhere>
2009-07-30 23:57:58 +0000

    Hmm.. maybe I should send this file in as it is.  I'm not sure if the current typing of 'get' is a bug or my failure to understand.

--- src/experimental/haskell_cnc/poly_col.hs
+++ src/experimental/haskell_cnc/poly_col.hs
@@ -1,7 +1,11 @@
-{-# LANGUAGE RankNTypes, ExistentialQuantification, ImpredicativeTypes #-}
+{-# LANGUAGE RankNTypes, ExistentialQuantification #-}
 
+-- , ScopedTypeVariables, PatternSignatures
+-- Also had that same 'impossible' crash on mac with 6.8.3
+
+--
+-- , ImpredicativeTypes
 -- 
--- , ScopedTypeVariables
 --  FlexibleContexts
 {- # OPTIONS glasgow-exts #-}
 
@@ -30,32 +34,19 @@ data MatchedTagMap  = forall a.   MT (Map (TagColID  a)   (TagCol a))
 
 type TagColID  a   = Int
 type ItemColID a b = Int
+type TagCol    a   = Set a
+type ItemCol   a b = Map a b
 
-type TagCol a    = Set a
-type ItemCol a b = Map a b
-
--- A STEP:
 -- Produces a batch of new data to write, or blocks
 type Step a = Collections -> a -> StepResult
 data StepResult = Done ([NewTag], [NewItem])
-                | forall a. Block (TagColID a) a
+                | forall a. Ord a => Block (TagColID a) a
 		  -- type-wise Block is the same as NewTag
 data NewTag  = forall a.   Ord a => NT (TagColID  a)   a
 data NewItem = forall a b. Ord a => NI (ItemColID a b) a b
 
-
--- A GRAPH:
--- Separate from the collections themselves, the graph keeps track of
--- the prescribed steps.  Needs ImpredicativeTypes:
---data Graph = G [forall a. (TagColID a, Step a)]
---data Graph = forall a. G [(TagColID a, Step a)]
---data Graph a = G [(TagColID a, Step a)]
-
---data MatchedStep = forall a. MS (TagColID a, Step a)
---type Graph = [MatchedStep]
-
 -- Need it to be a map... but this type is wrong, as above:
-data Graph = forall a. G (Map (TagColID a) (Step a))
+data Graph = forall a. G (Map (TagColID a) [Step a])
 
 ------------------------------------------------------------
 -- (Optional) type signatures for operations:
@@ -64,8 +55,6 @@ newCollections :: IO (IORef Collections)
 newTagCol      :: IORef Collections -> IO (TagColID a)
 newItemCol     :: IORef Collections -> IO (ItemColID a b)
 
-
-get :: Ord a => Collections -> ItemColID a b -> a -> Maybe b
 
 -- These are called by the step code and produce outbound tags and items:
 put  :: Ord a => ItemColID a b -> a -> b -> NewItem
@@ -85,106 +74,80 @@ newItemCol ref = do (cnt, tags, MI items) <- readIORef ref
 		    writeIORef ref (cnt+1, tags, MI newitems)
 		    return cnt
 
---get (MT tmap, MI imap) (id :: ItemColID a b) tag = 
-get (_, MT tmap, MI imap) id tag = 
-  Map.lookup tag (unsafeCoerce (imap!id))
+--get (_, MT tmap, MI imap) (id :: TagColID a)  tag = 
 
- -- Should we be strict in this outer lookup??
---   case imap ! id of 
---     items -> Map.lookup tag (unsafeCoerce items)
+get :: Ord a => Collections -> ItemColID a b -> a -> Maybe b
+--get (_, MT tmap, MI imap) (id :: ItemColID a b)  tag = 
+get (_, MT tmap, MI imap) (id )  tag = 
+  let itemcol = imap!id in
+  Map.lookup tag (unsafeCoerce itemcol )
 
-
---  case Map.lookup id imap of 
---    Nothing -> Nothing
---    Just items -> Map.lookup tag (unsafeCoerce items)
-
--- Having problems with scopedtypevariables
---    Just items -> Map.lookup tag (items :: ItemCol a b)
-
--- Just accumulate puts:
-put id tag item = NI id tag item
+put id tag item = NI id tag item -- Just accumulate puts as data
 call id tag     = NT id tag 
 
-
--- SHOULD WE USE foldl' ???
-
 -- This inserts new items and tags into a Collections object.
-
+--
 -- This is inefficient in that it looks up the tagCol/itemCol ID for
 -- each update.  Ideally, steps would produce a more organized
 -- "chunked" structure so that we could
+--
+-- Also, we could optimize this here by optimistically assuming that a
+-- batch of updates are likely to the same collection.
 
--- Also, we could optimize this here by optimistically assuming that 
-
-mergeUpdates cref newtags newitems =
-    do (n, MT tags, MI items) <- readIORef cref
+--mergeUpdates :: IORef Collections -> [NewTag] -> [NewItem] -> IO ()
+mergeUpdates :: [NewTag] -> [NewItem] -> Collections -> Collections
+--mergeUpdates cref newtags newitems =
+mergeUpdates newtags newitems (n, MT tags, MI items) =
+       -- SHOULD WE USE foldl' ???
        let items' = foldl (\ acc (NI id k x) -> 
  			  -- DOESNT SEEM LIKE WE SHOULD NEED TWO unsafeCoerces!!
   			  let _acc = unsafeCoerce acc 
  			      col = Map.insert k x (_acc!id) in
   			  unsafeCoerce $ Map.insert id col _acc)
- 	             items newitems
+ 	             items newitems in
        let tags' = foldl (\ acc (NT id k) -> 
  			  let _acc = unsafeCoerce acc 
  			      col = Set.insert k (_acc!id) in
  			  unsafeCoerce $ Map.insert id col _acc)
- 	             tags newtags
-       writeIORef cref (n, MT tags', MI items')
-       return ()
-
---        let itemloop acc [] = items
--- 	   itemloop acc ((NI id k x):tl) = 
--- 	       let _acc = unsafeCoerce acc 
--- 		   col = Map.insert k x (_acc!id)
--- 		   acc' = unsafeCoerce $ Map.insert id col _acc 
--- 	       in  itemloop acc' tl
-
---        let items'' = itemloop items newitems 
---        let tags' = tags 
-
-
-
-
---prescribe :: Graph a -> TagColID a -> Step a -> Graph a
---prescribe :: Graph -> TagColID a -> Step a -> Graph
---prescribe (G graph) tagsid step = G ((tagsid,step) : graph )
-
---prescribe :: Graph -> (TagColID a, Step a) -> Graph
---prescribe (G graph) pair = G (pair : graph )
-
---prescribe :: Graph -> MatchedStep -> Graph
---prescribe graph pair = (pair : graph )
-
-
-
---emptyGraph = []
---prescribe :: Graph -> TagColID a -> Step a -> Graph
---prescribe graph id step = MS (id,step) : graph 
+ 	             tags newtags in
+       (n, MT tags', MI items')
+--       writeIORef cref 
+--       return ()
 
 emptyGraph = G Map.empty
 prescribe :: TagColID a -> Step a -> Graph -> Graph
-prescribe id step (G gmap) = G (Map.insert id (unsafeCoerce step) gmap)
+prescribe id step (G gmap) = G (Map.insertWith (++) id [unsafeCoerce step] gmap)
 
 -- Retrieve the steps from a graph:
---getSteps :: Graph -> TagColID a -> [Step a]
+getSteps :: Graph -> TagColID a -> [Step a]
+getSteps (G graph) id = Map.findWithDefault [] id (unsafeCoerce graph)
 
 -- Serially run actions and make updates
-serialScheduler cref (G graph) inittags = 
-    do c <- readIORef cref
+serialScheduler graph inittags cols = schedloop cols [] inittags
+--    do c <- readIORef cref       
+--	    schedloop c [] inittags
        -- Initially we should take ALL resident tags to be un-processed:
        -- Nevermind, we are lazy and require the user tell us:
-       let schedloop [] = return ()
-	   schedloop (NT id tag : tl) = 
---	     let 
-	     case undefined of --step tag of          
-	     Block id tag -> return ()
-       return ()
+ where schedloop c [] [] = c
+       -- FIXME: TEMP HACK -- just try all the blocked ones when we run out of other stuff:
+       -- TODO: wake up blocked steps intelligently when the output is produced.
+       schedloop c blocked [] = schedloop c [] blocked
+       schedloop c blocked (NT id tag : tl) = 
+	     foldl (\ acc step -> 
+		    case step acc tag of
+		      Block id tag -> schedloop acc ((NT id tag):blocked) tl
+		      Done (newtags, newitems) -> 
+		        schedloop (mergeUpdates newtags newitems acc)
+		                  blocked (newtags++tl) -- FIXME: SUPPRESS pre-existing tags. Currently the tag collections do NOTHING
+		   )
+	           c (getSteps graph id)
+
 
 --------------------------------------------------------------------------------
 -- Test program:
 
-type TI = TagColID Int
-type II = ItemColID Int Int
+type TI = TagColID  Char
+type II = ItemColID Char Int
 incrStep :: TI -> (TI, II) -> Step Int
 incrStep d1 (t2,d2) c tag = 
     case get c d1 tag of 
@@ -197,27 +160,30 @@ test = -- Allocate collections:
        t1 <- newTagCol cref
        t2 <- newTagCol cref
        t3 <- newTagCol cref
-       d1 <- newItemCol cref
+       d1 <- newItemCol cref :: IO (ItemColID Char Int)
        d2 <- newItemCol cref
        d3 <- newItemCol cref
-       c  <- readIORef cref
 
        -- Initialize:
-       mergeUpdates cref
-		    []
-		    [put d1 'a' 33,
-		     put d1 'b' 100]
-
+       modifyIORef cref $ 
+	 mergeUpdates [] [put d1 'a' 33, 
+			  put d1 'b' 100]
+{-
        let graph = 
 	    prescribe t1 (incrStep d1 (t2,d2)) $
 	    prescribe t2 (incrStep d2 (t3,d3)) $
 	    emptyGraph
 
        let inittags = [call t1 'a',  call t1 'b']
---       serialScheduler cref graph inittags
 
+       modifyIORef cref $ serialScheduler graph inittags
+
+-}
        c  <- readIORef cref
-       return (get c d3 'a', get c d3 'b') 
+       let foo = get c d1 'a' :: Maybe Int
+       return $ (d1, foo)
+--       return (get c d1 'a', get c d1 'b') 
+--       return (get c d3 'a', get c d3 'b') 
       
 
 
