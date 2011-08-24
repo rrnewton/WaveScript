d93821b26034b847194de38331b53f9f9ae66665
Ryan Newton <rrnewton@gmail.com>
2011-08-24 15:31:13 -0400

    Reproduced github issue #5 in test 7B_MergeFiles.  Namely, not all the tuples are produced.

--- apps/reuters/runtime/tests/7B_MergeFiles.c
+++ apps/reuters/runtime/tests/7B_MergeFiles.c
@@ -17,9 +17,13 @@ int main(int argc, char* argv[]) {
 
       // Tuples, 100Hz
       // Should be EXACTLY 10 tuples from this file:
-      WSQ_AddOp(1, "ASCIIFileSource", "", "2", "-1 |foobar.schema|./example_distributed/taq_10lines.dat"); 
+      WSQ_AddOp(1, "ASCIIFileSource", "", "100", "-1 |foobar.schema|./example_distributed/taq_10lines.dat"); 
 
-      WSQ_AddOp(2, "Printer", "2", "", "From file source:");
+      WSQ_AddOp(2, "ASCIIFileSource", "", "200", "-1 |foobar.schema|./example_distributed/taq_10lines.dat"); 
+
+      WSQ_AddOp(3, "MergeMonotonic", "100 200", "300", "RECEIVEDTIME");
+
+      WSQ_AddOp(4, "Printer", "300", "", "Merged file sources:");
 
     WSQ_EndSubgraph();
   WSQ_EndTransaction();
