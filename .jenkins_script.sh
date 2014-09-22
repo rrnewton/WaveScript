b3e861e4685927d1a955968c25b8d905fdeaee8c
Ryan Newton <rrnewton@gmail.com>
2014-09-22 11:43:40 -0400

    Add beginnings of a jenkins testing scritp

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo 'FINISH this Jenkins script!'
+if [ "$R6RSVER" == chez ]; then
+
+    cd src
+    make 
+
+elif [ "$R6RSVER" == plt ]; then
+
+    cd src
+    make wsparse
+    make aggregated
+    make pltbc
+
+else
+    echo "R6RSVER is not set to a known setting! ($R6RSVER)"
+    exit 1
+fi 
