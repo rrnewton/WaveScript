c31c8e6ef22056027d4f6479a99c336d26be1849
Michael DeWitt <md@hackfoolery.com>
2014-10-27 09:39:16 -0400

    HSBencher harness w/o any plugins

--- Makefile
+++ Makefile
@@ -22,16 +22,26 @@ clean:
 # Benchmarking stuff
 ####################
 
-.phony: bench
-bench: bench_deps
-	./run_benchmarks.exe
+ifeq ($(CABAL),)
+  CABAL=cabal
+endif
+ifeq ($(MACHINECLASS),)
+  MACHINECLASS=$(shell hostname -s)
+endif
 
-bench_deps: run_benchmarks.exe default
+RUNID=$(shell hostname -s)_$(shell date "+%s")
 
-run_benchmarks.exe: run_benchmarks.cabal run_benchmarks.hs
-	cabal sandbox init
-	cabal install ./HSBencher/hsbencher ./HSBencher/hsbencher-fusion -j
-	cabal install --bindir=. --program-suffix=.exe ./
+ifeq ($(JENKINS_GHC),)
+  JENKINS_GHC=7.8.3
+endif
+
+PKGS= ./ ./HSBencher/hgdata ./HSBencher/hsbencher ./HSBencher/hsbencher-tool #./HSBencher/hsbencher-fusion ./HSBencher/hsbencher-codespeed
+CBLARGS= --disable-documentation  --with-ghc=ghc-$(JENKINS_GHC) --force-reinstalls
+
+bench_deps: run_benchmarks.cabal run_benchmarks.hs
+	which -a $(CABAL)
+	$(CABAL) sandbox init
+	$(CABAL) install $(CBLARGS) --bindir=. $(PKGS)
 
 clean_bench:
 	rm run_benchmarks.exe
--- run_benchmarks.hs
+++ run_benchmarks.hs
@@ -1,91 +1,53 @@
-#!/usr/bin/env runghc
 {-# LANGUAGE NamedFieldPuns #-}
 
--- | This script runs all Harlan benchmarks.  It is based on a Haskell
--- benchmarking framework called HSBencher.  Its main advantage is
--- that it supports uploading of benchmark timings to a Google Fusion
--- Table.
-
--- Requires hsbencher >= 0.2
+-- | HSBencher script to run all the benchmarks.
+module Main where
 
 import Control.Monad
 import Data.Maybe
 import qualified Data.ByteString.Char8 as B
-import System.Directory
-import System.FilePath
 import System.Exit
-import System.Environment (getArgs)
-import System.Process
-import GHC.Conc (getNumProcessors)
-import System.IO.Unsafe (unsafePerformIO)
-import Debug.Trace
 
 import HSBencher (defaultMainModifyConfig)
 import HSBencher.Types
-import HSBencher.Methods (makeMethod)
-import HSBencher.Logging (log)
-import HSBencher.MeasureProcess
-import HSBencher.Utils (runLogged, defaultTimeout)
+import HSBencher.Internal.Utils (runLogged, defaultTimeout)
 
-import HSBencher.Backend.Fusion  (defaultFusionPlugin)
-import HSBencher.Backend.Dribble (defaultDribblePlugin)
+--import HSBencher.Backend.Fusion  (defaultFusionPlugin)
 
 import Prelude hiding (log)
 --------------------------------------------------------------------------------
 
 main :: IO ()
-main = defaultMainModifyConfig myconf
+main = defaultMainModifyConfig $ \conf -> conf
+  { benchlist = benches
+  , buildMethods = [ wsc2 ]
+  , plugIns = []
+  }
 
-all_benchmarks :: [Benchmark DefaultParamMeaning]
-all_benchmarks =
-  [ mkBenchmark "test/bench-add-vector.kfc" [show sz] defaultCfgSpc  | sz <- [1,3 .. 90] ] ++ 
-  [ mkBenchmark "test/bench-nbody.kfc"      [show sz] defaultCfgSpc  | sz <- [1 .. 66] ] ++ 
-  [ mkBenchmark "test/bench-mandelbrot.kfc"      [show sz] defaultCfgSpc  | sz <- [1 .. 50] ] ++
-  [ mkBenchmark "test/bench-bfs-color.kfc" [show sz, "16"] defaultCfgSpc 
-  | sz <- [100, 1000, 10^4, 10^5, 10^5] ] ++
-  [ mkBenchmark "test/bench-raytrace.kfc" [show flag, "100"] defaultCfgSpc | flag <- [0,1] ]
+benches :: [Benchmark DefaultParamMeaning]
+benches = [ mkBenchmark (bench "pipline/num_pipe.ws") tuples compileSpec
+            | tuples <- runtimeSpec ]
+  where bench = ("benchmarks/microbenchmarks/" ++)
+        runtimeSpec = [ ["-n", show n] | n <- [40,80 .. 400] ]
+        compileSpec = Or [ Set NoMeaning . RuntimeEnv "NUMOPS" $ show ops
+                           | ops <- [16,32 .. 400] ]
 
--- Default configuration space over which to vary settings:
-defaultCfgSpc = Or [gpu]
- where
-   cpu = And [ Set (Variant "CPU") (RuntimeEnv "HARLAN_DEVICE" "cpu") ]
-   gpu = And [ Set (Variant "GPU") (RuntimeEnv "HARLAN_DEVICE" "gpu")
-             , Set NoMeaning (RuntimeEnv "HARLAN_MIN_REGION_SIZE" "134217728") ]
+wsc2 :: BuildMethod
+wsc2 = BuildMethod
+  { methodName = "wsc2"
+  , canBuild = WithExtension ".ws"
+  , concurrentBuild = False
+  , setThreads = Nothing
+  , clean = \_ _ _ -> return ()
+  , compile = \_ _ _ target -> do
+      runSuccessful " [wsc2] " $ "wsc2 " ++ target
+      return $ StandAloneBinary "./query.exe"
+  }
 
--- | Put it all together as a full HSBencher configuration.
-myconf :: Config -> Config
-myconf conf =
-  conf
-   { benchlist = all_benchmarks
-   , buildMethods = [ harlanMethod ]
-   , plugIns   = [ SomePlugin defaultFusionPlugin,
-                   SomePlugin defaultDribblePlugin ]
-   }
-
--- | Teach HSBencher how to build Harlan files From HSBencher's
--- perspective, it just runs pre-built binaries compiled with harlanc.
-harlanMethod :: BuildMethod
-harlanMethod = BuildMethod
-      { methodName = "harlanMethod"
-      , canBuild = WithExtension ".kfc"
-      , concurrentBuild = False
-      , setThreads      = Nothing
-      , clean = \_ _ _ -> return ()
-      , compile = \ _ _ _ targetpath -> do
-         log$ tag++" Compiling harlan file: "++targetpath
-         runSuccessful " [harlanc] " ("./harlanc "++targetpath)
-         return (StandAloneBinary ("./" ++ (takeBaseName targetpath)))
-      }
- where
-    tag = " [run-benchmarks] "
-
-
--- | A simple wrapper for a command that is expected to succeed (and whose output we
--- don't care about).  Throws an exception if the command fails.
 runSuccessful :: String -> String -> BenchM [B.ByteString]
 runSuccessful tag cmd = do
   (res,lines) <- runLogged tag cmd
   case res of
-    ExitError code  -> error$ "expected this command to succeed! But it exited with code "++show code++ ":\n  "++ cmd
+    ExitError code  -> error $ "expected this command to succeed! But it exited with code "++show code++ ":\n  "++ cmd
     RunTimeOut {}   -> error "Methods.hs/runSuccessful - internal error!"
     RunCompleted {} -> return lines
